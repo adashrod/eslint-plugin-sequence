c717a47b003e641364b22a18a850723ed2b4006a
Aaron Rodriguez <adashrod@gmail.com>
2022-03-10 23:14:07 -0800

    enable commented out TS-only tests in ordered-imports-by-path

--- src/tests/lib/rules/ordered-imports-by-path.js
+++ src/tests/lib/rules/ordered-imports-by-path.js
@@ -1,67 +1,58 @@
 const rule = require("../../../lib/rules/ordered-imports-by-path");
-var RuleTester = require("eslint").RuleTester;
-const ruleTester = new RuleTester({
+const RuleTester = require("eslint").RuleTester;
+
+const esRuleTester = new RuleTester({
     parserOptions: {
         ecmaVersion: 6,
         sourceType: "module",
     }
 });
 
-ruleTester.run("ordered-imports-by-path", rule, {
+esRuleTester.run("ordered-imports-by-path", rule, {
     valid: [
-        `import Alpha from "Alpha";\n` + 
+        `import Alpha from "Alpha";\n` +
             `import Bravo from "Bravo";`,
 
-        `import Alpha from "Alpha";\n` + 
-            `import { Bravo } from "Bravo";\n` + 
+        `import Alpha from "Alpha";\n` +
+            `import { Bravo } from "Bravo";\n` +
             `import { Charlie } from "Charlie";\n` +
             `import Delta from "Delta";`,
         {
-            code: `import { Alpha } from "Alpha";\n` + 
+            code: `import { Alpha } from "Alpha";\n` +
                 `import Charlie from "Charlie";\n` +
                 `import bravo from "bravo";\n`,
             options: [{ignoreCase: false}]
         }, {
-            code: `import { Alpha } from "Alpha";\n` + 
-                `import bravo from "bravo";\n` + 
+            code: `import { Alpha } from "Alpha";\n` +
+                `import bravo from "bravo";\n` +
                 `import Charlie from "Charlie";\n`,
             options: [{ignoreCase: true}]
         }, {
-            code: `import { Alpha } from "Alpha";\n` + 
-                `import Bravo from "Bravo";\n` + 
+            code: `import { Alpha } from "Alpha";\n` +
+                `import Bravo from "Bravo";\n` +
                 `import Charlie from "Charlie";\n`,
             options: [{allowSeparateGroups: false}]
         }, {
-            code: `import { Alpha } from "Alpha";\n` + 
-                `import Bravo from "Bravo";\n` + 
+            code: `import { Alpha } from "Alpha";\n` +
+                `import Bravo from "Bravo";\n` +
                 `import Charlie from "Charlie";\n\n` +
-        
-                `import Alice from "Alice";\n` + 
+
+                `import Alice from "Alice";\n` +
                 `import Bob from "Bob";\n`,
             options: [{allowSeparateGroups: true}]
         }, {
-            code: `import { Alpha } from "Alpha";\n` + 
-                `import Bravo from "Bravo";\n` + 
+            code: `import { Alpha } from "Alpha";\n` +
+                `import Bravo from "Bravo";\n` +
                 `import "Cool-script";\n` +
                 `import Delta from "Delta";\n`,
             options: [{sortSideEffectsFirst: false}]
         }, {
             code: `import "Cool-script";\n` +
-                `import { Alpha } from "Alpha";\n` + 
-                `import Bravo from "Bravo";\n` + 
+                `import { Alpha } from "Alpha";\n` +
+                `import Bravo from "Bravo";\n` +
                 `import Charlie from "Charlie";\n`,
             options: [{sortSideEffectsFirst: true}]
-        }, 
-        // TypeScript-only, todo: find a parser config to support these
-        // {
-        //     code: `import { ArrayList } from "Collections";\n` +
-        //         `import type { List } from "Collections";\n`,
-        //     options: [{sortTypeImportsFirst: false}]
-        // }, {
-        //     code: `import type { List } from "Collections";\n` +
-        //         `import { ArrayList } from "Collections";\n`,
-        //     options: [{sortTypeImportsFirst: true}]
-        // },
+        },
         `import { Alpha, Bravo, Charlie, Delta } from "alphabet";`,
         `import {\n` +
             `Alpha, // first letter\n` +
@@ -89,41 +80,41 @@ ruleTester.run("ordered-imports-by-path", rule, {
             `Echo } from "alphabet";`,
     ],
     invalid: [{
-        code: `import Bravo from "Bravo";\n` + 
+        code: `import Bravo from "Bravo";\n` +
             `import Alpha from "Alpha";`,
         errors: [{
             messageId: "sortImportsByPath"
         }],
-        output: `import Alpha from "Alpha";\n` + 
+        output: `import Alpha from "Alpha";\n` +
             `import Bravo from "Bravo";`
     }, {
-        code: `import Bravo from "Bravo";\n` + 
+        code: `import Bravo from "Bravo";\n` +
             `import alphaFun from "alphaFun";`,
         options: [{ignoreCase: true}],
         errors: [{
             messageId: "sortImportsByPath"
         }],
-        output: `import alphaFun from "alphaFun";\n` + 
+        output: `import alphaFun from "alphaFun";\n` +
             `import Bravo from "Bravo";`
     }, {
-        code: `import { Alpha } from "Alpha";\n` + 
-            `import Bravo from "Bravo";\n` + 
+        code: `import { Alpha } from "Alpha";\n` +
+            `import Bravo from "Bravo";\n` +
             `import Charlie from "Charlie";\n\n` +
-    
-            `import Alice from "Alice";\n` + 
+
+            `import Alice from "Alice";\n` +
             `import Bob from "Bob";\n`,
         options: [{allowSeparateGroups: false}],
         errors: [{
             messageId: "sortImportsByPath"
         }],
         output: `import Alice from "Alice";\n` +
-            `import { Alpha } from "Alpha";\n` + 
+            `import { Alpha } from "Alpha";\n` +
             `import Bob from "Bob";\n` +
-            `import Bravo from "Bravo";\n` + 
+            `import Bravo from "Bravo";\n` +
             `import Charlie from "Charlie";\n`
     }, {
-        code: `import { Alpha } from "Alpha";\n` + 
-            `import Bravo from "Bravo";\n` + 
+        code: `import { Alpha } from "Alpha";\n` +
+            `import Bravo from "Bravo";\n` +
             `import "Cool-script";\n` +
             `import Delta from "Delta";\n`,
         options: [{sortSideEffectsFirst: true}],
@@ -131,42 +122,55 @@ ruleTester.run("ordered-imports-by-path", rule, {
             messageId: "sortSideEffectsFirst"
         }],
         output: `import "Cool-script";\n` +
-            `import { Alpha } from "Alpha";\n` + 
-            `import Bravo from "Bravo";\n` + 
+            `import { Alpha } from "Alpha";\n` +
+            `import Bravo from "Bravo";\n` +
             `import Delta from "Delta";\n`
     }, {
         code: `import "Cool-script";\n` +
-            `import { Alpha } from "Alpha";\n` + 
-            `import Bravo from "Bravo";\n` + 
+            `import { Alpha } from "Alpha";\n` +
+            `import Bravo from "Bravo";\n` +
             `import Delta from "Delta";\n`,
         options: [{sortSideEffectsFirst: false}],
         errors: [{
             messageId: "sortImportsByPath"
         }],
-        output: `import { Alpha } from "Alpha";\n` + 
-            `import Bravo from "Bravo";\n` + 
+        output: `import { Alpha } from "Alpha";\n` +
+            `import Bravo from "Bravo";\n` +
             `import "Cool-script";\n` +
             `import Delta from "Delta";\n`
-    // TypeScript-only, todo: find a parser config to support these
-    // }, {
-    //     code: `import type { List } from "Collections";\n` +
-    //         `import { ArrayList } from "Collections";\n`,
-    //     options: [{sortTypeImportsFirst: false}]
-    //     errors: [{
-    //         messageId: "sortTypeImports"
-    //     }],
-    //     output: `import { ArrayList } from "Collections";\n` +
-    //         `import type { List } from "Collections";\n`
-    // }, {
-    // {
-    //     code: `import { ArrayList } from "Collections";\n` +
-    //         `import type { List } from "Collections";\n`,
-    //     options: [{sortTypeImportsFirst: true}],
-    //     errors: [{
-    //         messageId: "sortTypeImports"
-    //     }],
-    //     output: `import type { List } from "Collections";\n` +
-    //         `import { ArrayList } from "Collections";\n`
-    // }
+    }]
+});
+
+const tsRuleTester = new RuleTester({
+    parser: require.resolve('@typescript-eslint/parser')
+});
+tsRuleTester.run("", rule, {
+    valid: [{
+        code: `import { ArrayList } from "Collections";\n` +
+            `import type { List } from "Collections";\n`,
+        options: [{sortTypeImportsFirst: false}]
+    }, {
+        code: `import type { List } from "Collections";\n` +
+            `import { ArrayList } from "Collections";\n`,
+        options: [{sortTypeImportsFirst: true}]
+    }],
+    invalid: [{
+        code: `import type { List } from "Collections";\n` +
+            `import { ArrayList } from "Collections";\n`,
+        options: [{sortTypeImportsFirst: false}],
+        errors: [{
+            messageId: "sortTypeImports"
+        }],
+        output: `import { ArrayList } from "Collections";\n` +
+            `import type { List } from "Collections";\n`
+    }, {
+        code: `import { ArrayList } from "Collections";\n` +
+            `import type { List } from "Collections";\n`,
+        options: [{sortTypeImportsFirst: true}],
+        errors: [{
+            messageId: "sortTypeImports"
+        }],
+        output: `import type { List } from "Collections";\n` +
+            `import { ArrayList } from "Collections";\n`
     }]
 });
